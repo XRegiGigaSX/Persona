ac1441a7d1a5e4841ac9c1e8c2fe57e7da6b39ea
Rayyan Shaikh <rayyan.shaikh01@gmail.com>
2023-06-21 22:19:07 +0530

    trying fix

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -64,7 +64,7 @@ const ComputersCanvas = () => {
           maxPolarAngle={Math.PI/2}
           minPolarAngle={Math.PI/2}
         />
-        <Computers isMobile={isMobile} />
+        <Computers />
       </Suspense>
 
       {/* <Preload all /> */}
